5718359ddab9f55e8c39a01e22f61f304f4260de
gurmanpreet nagra <gurmanpreetnagra@gmail.com>
2022-08-10 16:06:56 +0100

    added html for time blocks

--- main/assets/script.js
+++ main/assets/script.js
@@ -1,2 +1,28 @@
 // Current Day
 $("#currentDay").text(moment().format("dddd, MMMM Do"));
+
+// render working blocks
+const workingBlock = $("#timeBlock");
+const renderWorkingBlock = (hour) => {
+  console.log(hour);
+  const timeBlock = `<div class="row p-2 my 2 ${getClassName(container.key)}">
+  <div class="col-md-1 col-sm-12 text-center my-1 d-flex flex-column justify-content-center">${
+    container.label
+  }</div>
+  <textarea data-info=${
+    container.key
+  } class="col-md-9 col-sm-12" rows="3">${getEventForTimeBlock(
+    workingHour.key
+  )}</textarea>
+  <div class="col-md-2 col-sm-12 text-center my-1 d-flex flex-column justify-content-center">
+    <button type="button" data-hour=${
+      container.key
+    } class="btn btn-outline-success">Save</button>
+  </div>`;
+
+  //append to parent time block
+  workingBlock.append(timeBlock);
+};
+container.forEach(renderTimeBlock);
+
+workingBlocks.on("click", handleSave);
